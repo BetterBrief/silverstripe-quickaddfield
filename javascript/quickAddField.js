17b4bf5350386b45831a20e7f106b97661f59bb1
unknown <dan@hens.by>
2011-08-01 17:18:52 +0100

    JS now lets you click the add button :P

--- javascript/quickAddField.js
+++ javascript/quickAddField.js
@@ -1,57 +1,63 @@
 (function($) {
-	$('input.quickadd').live('keypress',function(e) {
-		if (e.keyCode == 13) {
-			var $this = $(this),
-				$holder = $this.closest('div.quickAddHolder'),
-				$options = $('ul.optionset',$holder),
-				$inputs = $('input',$options),
-				fieldName = $holder.children('div.quickadd:first').attr('id');
-			$.ajax({
-				beforeSend: function(XHR,settings) {
-					if ($this.data('inProgress')) {
-						return false;
+	$quickAddInput = $('input.quickadd');
+	$('a.quickadd').live('click',function() {
+		var $this = $(this),
+			$holder = $this.closest('div.quickAddHolder'),
+			$options = $('ul.optionset',$holder),
+			$inputs = $('input',$options),
+			fieldName = $holder.children('div.quickadd:first').attr('id'),
+			$quickAddInput = $this.parent().prev().find('input.quickadd');
+		$.ajax({
+			beforeSend: function(XHR,settings) {
+				if ($this.data('inProgress')) {
+					return false;
+				}
+				$this.data('inProgress',true);
+			},
+			complete: function(XHR,textStatus) {
+				$this.data('inProgress',false);
+			},
+			data: {'Title':$quickAddInput.val()},
+			dataType: 'json',
+			error: function(XHR,textStatus,errorThrown) {
+			},
+			success: function(data,textStatus,XHR) {
+				var $input = $inputs.filter('[value="' + data.ID + '"]');
+				if ($input.length) {
+					if (!$input.is(':checked')) {
+						$input.click();
 					}
-					$this.data('inProgress',true);
-				},
-				complete: function(XHR,textStatus) {
-					$this.data('inProgress',false);
-				},
-				data: {'Title':$this.val()},
-				dataType: 'json',
-				error: function(XHR,textStatus,errorThrown) {
-				},
-				success: function(data,textStatus,XHR) {
-					var $input = $inputs.filter('[value="' + data.ID + '"]');
-					if ($input.length) {
-						if (!$input.is(':checked')) {
-							$input.click();
-						}
+				}
+				else {
+					if (!$inputs.length) {
+						var inputType = $options.hasClass('checkboxsetfield') ? 'checkbox' : 'radio',
+							$new = $('<li class="val' + data.ID + ' odd"><input type="' + inputType + '" id="' + $options.attr('id') + '_' + data.ID + '" name="' + fieldName + '[' + data.ID + ']' + '" value="' + data.ID + '" checked="checked" /><label for="' + $options.attr('id') + '_' + data.ID + '">' + data.Title + '</label></li>');
+						$options.children().remove();
 					}
 					else {
-						if (!$inputs.length) {
-							var inputType = $options.hasClass('checkboxsetfield') ? 'checkbox' : 'radio',
-								$new = $('<li class="val' + data.ID + ' odd"><input type="' + inputType + '" id="' + $options.attr('id') + '_' + data.ID + '" name="' + fieldName + '[' + data.ID + ']' + '" value="' + data.ID + '" checked="checked" /><label for="' + $options.attr('id') + '_' + data.ID + '">' + data.Title + '</label></li>');
-							$options.children().remove();
+						var $new = $options.children(':last').clone(),
+							val = $new.val();
+						$new.removeClass('val' + val).addClass('val' + data.ID);
+						if ($new.hasClass('even')) {
+							$new.removeClass('even').addClass('odd');
 						}
 						else {
-							var $new = $options.children(':last').clone(),
-								val = $new.val();
-							$new.removeClass('val' + val).addClass('val' + data.ID);
-							if ($new.hasClass('even')) {
-								$new.removeClass('even').addClass('odd');
-							}
-							else {
-								$new.addClass('even').removeClass('odd');
-							}
-							$new.children('input').attr('id',$options.attr('id') + '_' + data.ID).attr('name',fieldName + '[' + data.ID + ']').val(data.ID).attr('checked','checked');
-							$new.children('label').attr('for',$options.attr('id') + '_' + data.ID).text(data.Title);
+							$new.addClass('even').removeClass('odd');
 						}
-						$options.append($new);
-						$this.val('');
+						$new.children('input').attr('id',$options.attr('id') + '_' + data.ID).attr('name',fieldName + '[' + data.ID + ']').val(data.ID).attr('checked','checked');
+						$new.children('label').attr('for',$options.attr('id') + '_' + data.ID).text(data.Title);
 					}
-				},
-				url: $this.closest('form').attr('action') + '/field/' + fieldName + '/findOrAdd'
-			});
+					$options.append($new);
+					$quickAddInput.val('');
+				}
+			},
+			url: $this.closest('form').attr('action') + '/field/' + fieldName + '/findOrAdd'
+		});
+		return false;
+	});
+	$quickAddInput.live('keypress',function(e) {
+		if (e.keyCode == 13) {
+			$(this).parent().next().find('a.quickadd').click();
 		}
 	});
 })(jQuery)
